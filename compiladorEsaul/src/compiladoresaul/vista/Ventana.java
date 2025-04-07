24dd4a3f2c0df2d701bc1415b3986c23fd13e9c1
ManuelLopez12 <l21450188@itz.edu.mx>
2025-04-07 08:10:13 -0600

    entrega del lunes

--- compiladorEsaul/src/compiladoresaul/vista/Ventana.java
+++ compiladorEsaul/src/compiladoresaul/vista/Ventana.java
@@ -11,13 +11,11 @@ import controlador.Control;
  * @author JM LOPEZ HURTADO
  */
 public class Ventana extends javax.swing.JFrame {
-   private Control c; 
-    /**
-     * Creates new form ventana
-     */
+private Control c;
+    
     public Ventana() {
         initComponents();
-         c = new Control();
+        c=new Control();
     }
 
     /**
@@ -29,93 +27,57 @@ public class Ventana extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        txtCodigo = new javax.swing.JTextArea();
+        txtMensaje = new javax.swing.JTextArea();
         jScrollPane2 = new javax.swing.JScrollPane();
         txtSalida = new javax.swing.JTextArea();
         jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        MnuAbrir = new javax.swing.JMenuItem();
+        mnuArchivo = new javax.swing.JMenu();
+        mnuAbrir = new javax.swing.JMenuItem();
         mnuLimpiar = new javax.swing.JMenuItem();
         mnuSalir = new javax.swing.JMenuItem();
-        compilar = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
         mnuLexico = new javax.swing.JMenuItem();
-
-        jMenuItem1.setText("jMenuItem1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        txtCodigo.setColumns(20);
-        txtCodigo.setRows(5);
-        jScrollPane1.setViewportView(txtCodigo);
+        txtMensaje.setColumns(20);
+        txtMensaje.setRows(5);
+        jScrollPane1.setViewportView(txtMensaje);
 
         txtSalida.setColumns(20);
         txtSalida.setRows(5);
         jScrollPane2.setViewportView(txtSalida);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane1)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 568, Short.MAX_VALUE)))
-                .addContainerGap())
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 171, Short.MAX_VALUE)
-                .addGap(46, 46, 46))
-        );
+        mnuArchivo.setText("Archivo");
 
-        jMenu1.setText("Archivo ");
-
-        MnuAbrir.setText("Abrir");
-        MnuAbrir.addActionListener(new java.awt.event.ActionListener() {
+        mnuAbrir.setText("Abrir");
+        mnuAbrir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                MnuAbrirActionPerformed(evt);
+                mnuAbrirActionPerformed(evt);
             }
         });
-        jMenu1.add(MnuAbrir);
+        mnuArchivo.add(mnuAbrir);
 
         mnuLimpiar.setText("Limpiar");
-        mnuLimpiar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                
-            }
-        });
-        jMenu1.add(mnuLimpiar);
+        mnuArchivo.add(mnuLimpiar);
 
         mnuSalir.setText("Salir");
-        mnuSalir.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                
-            }
-        });
-        jMenu1.add(mnuSalir);
+        mnuArchivo.add(mnuSalir);
 
-        jMenuBar1.add(jMenu1);
+        jMenuBar1.add(mnuArchivo);
 
-        compilar.setText("Compilar");
+        jMenu2.setText("Compilar");
 
-        mnuLexico.setText("lexico");
+        mnuLexico.setText("Lexico");
         mnuLexico.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-          
+                mnuLexicoActionPerformed(evt);
             }
         });
-        compilar.add(mnuLexico);
+        jMenu2.add(mnuLexico);
 
-        jMenuBar1.add(compilar);
+        jMenuBar1.add(jMenu2);
 
         setJMenuBar(jMenuBar1);
 
@@ -123,24 +85,35 @@ public class Ventana extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 511, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 206, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void MnuAbrirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MnuAbrirActionPerformed
+    private void mnuAbrirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuAbrirActionPerformed
         // TODO add your handling code here:
-        c.abrirArchivo(txtCodigo);
-    }//GEN-LAST:event_MnuAbrirActionPerformed
+        c.abrirArchivo(txtMensaje);
+    }//GEN-LAST:event_mnuAbrirActionPerformed
+
+    private void mnuLexicoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuLexicoActionPerformed
+        // TODO add your handling code here:
+        c.obtenerToken(txtMensaje, txtSalida);
+    }//GEN-LAST:event_mnuLexicoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -148,18 +121,16 @@ public class Ventana extends javax.swing.JFrame {
    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenuItem MnuAbrir;
-    private javax.swing.JMenu compilar;
-    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JMenuItem mnuAbrir;
+    private javax.swing.JMenu mnuArchivo;
     private javax.swing.JMenuItem mnuLexico;
     private javax.swing.JMenuItem mnuLimpiar;
     private javax.swing.JMenuItem mnuSalir;
-    private javax.swing.JTextArea txtCodigo;
+    private javax.swing.JTextArea txtMensaje;
     private javax.swing.JTextArea txtSalida;
     // End of variables declaration//GEN-END:variables
 }
--- compiladorEsaul/src/controlador/Control.java
+++ compiladorEsaul/src/controlador/Control.java
@@ -10,6 +10,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
@@ -19,36 +21,154 @@ import javax.swing.JTextArea;
  * @author JM LOPEZ HURTADO
  */
 public class Control {
+
     //aegunda revision 14/03/2025
     private Ventana v;
     private ArrayList<String> line;
-    
-    public void abrirArchivo(JTextArea txt){
+
+    public void abrirArchivo(JTextArea txt) {
         line = new ArrayList<>();
         String parrafo = "";
         JFileChooser muestra = new JFileChooser();
         int resultado = muestra.showOpenDialog(v);
-        if(resultado == JFileChooser.APPROVE_OPTION){
+        if (resultado == JFileChooser.APPROVE_OPTION) {
             File archivo = muestra.getSelectedFile();
             try {
                 BufferedReader br = new BufferedReader(new FileReader(archivo));
                 parrafo = br.readLine();
-                while (parrafo != null) {                    
+                while (parrafo != null) {
                     line.add(parrafo);
                     parrafo = br.readLine();
                 }
                 txt.setText("");
                 for (int i = 0; i < line.size(); i++) {
-                    txt.append(line.get(i)+"\n");
-                    
+                    txt.append(line.get(i) + "\n");
+
                 }
             } catch (IOException e) {
                 JOptionPane.showMessageDialog(null,
-                        "Error \n"+ e, "Error", JOptionPane.ERROR_MESSAGE);
+                        "Error \n" + e, "Error", JOptionPane.ERROR_MESSAGE);
             }
-            
-        } 
+
         }
     }
-    
 
+    public void obtenerToken(JTextArea texto, JTextArea txtSalida) {
+        ArrayList<String> id = new ArrayList<>();
+        ArrayList<String> aux = new ArrayList<>();// para los elementos repetidos 
+        String ExR = "0|[1-9]+|[a-zA-Z][a-zA-Z0-9_]+|\\.|\\+|\\*|\\/|\\-|\\,|\\;|\\<>|\\=|\\_";
+        Pattern compilar = Pattern.compile(ExR);
+        Matcher buscar = compilar.matcher(texto.getText());
+        while (buscar.find()) {
+            String elemento = buscar.group();
+            
+          boolean repeticiones=false;
+            for (int i = 0; i < aux.size(); i++) {
+                if(aux.get(i).equals(elemento)){
+                    repeticiones=true;
+                    break;
+                }
+            }
+            if(!repeticiones){
+            int token = obtenido(elemento);
+            id.add(String.format("%s\t%d\n", elemento, token));
+            aux.add(elemento);
+         }
+        }
+        txtSalida.setText("");
+
+        for (int i = 0; i < id.size(); i++) {
+            txtSalida.append(id.get(i));
+        }
+    }
+
+    private int obtenido(String elemento) {
+         //para las palabras reservadas 
+          switch(elemento){
+             case "const":
+                return 8;
+            case "var":
+                return 14;
+            case "proced":
+                return 16;
+            case "print":
+                return 34;
+            case "input":
+                return 36;
+            case "exec":
+                return 38;
+            case "if":
+                return 40;
+            case "while":
+                return 44;
+        }
+        //token para los numeros 
+        if (elemento.matches("[0-9]+")) {
+            return 1;
+        }
+        // token para los indentificadores 
+        if (elemento.matches("[a-zA-Z][a-zA-Z0-9_]*")) {
+            return 2;
+        }
+        //token para los separadores
+        switch (elemento) {
+            case ".":
+                return 4;
+            case ",":
+                return 6;
+            case ";":
+                return 12;
+            case "[":
+                return 30;
+            case "}":
+                return 32;
+            case "(":
+                return 56;
+            case ")":
+                return 58;
+            case ":":
+                return 42;
+
+        }
+        //para los operadores relacionales 
+        switch (elemento) {
+            case "==":
+                return 18;
+            case "<>":
+                return 20;
+            case "<":
+                return 22;
+            case ">":
+                return 24;
+            case "<=":
+                return 26;
+            case ">=":
+                return 28;
+        }
+        // para los aritmeticos 
+        switch(elemento){
+           case "+":
+                return 52;
+            case "-":
+                return 54;
+            case "*":
+                return 60;
+            case "/":
+                return 62; 
+        }
+       
+      
+        //para los de asignacion 
+        switch (elemento){
+              case "=":
+                return 10;
+            case "->":
+                return 48;
+            case "<-":
+                return 50;
+        }
+        return -1;
+
+    }
+
+}
