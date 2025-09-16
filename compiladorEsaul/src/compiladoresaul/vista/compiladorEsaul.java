9298662f9e50048d2d46b1364932a356c18bf1dd
ManuelLopez12 <l21450188@itz.edu.mx>
2025-09-15 22:18:31 -0600

    jajaj

--- compiladorEsaul/src/compiladoresaul/vista/compiladorEsaul.java
+++ compiladorEsaul/src/compiladoresaul/vista/compiladorEsaul.java
@@ -2,24 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package compiladoresaul;
-
-import compiladoresaul.vista.Ventana;
+package compiladoresaul.vista;
 
 /**
  *
  * @author JM LOPEZ HURTADO
  */
-public class CompiladorEsaul {
+public class compiladorEsaul {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-           Ventana v = new Ventana();
-           v.setVisible(true);
+        Ventana v = new Ventana();
+        v.setVisible(true);
     }
     
 }
